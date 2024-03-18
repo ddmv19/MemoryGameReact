8a84fdab2c925ae59bd1a0bca134e1634dd28f11
ddmv19 <moronvillanuevadiego@outlook.com>
2024-03-18 01:16:13 -0500

    Init project

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import '@fontsource/fredoka-one'
+
+function App() {
+  return (
+    <main className="">
+      <h1
+        className="py-4 text-4xl text-center uppercase tracking-[.2em] border-b-4 border-slate-400"
+        style={{ fontFamily: 'Fredoka One' }}
+      >
+        Memory Game
+      </h1>
+    </main>
+  )
+}
+
+export default App
