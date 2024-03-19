0a8c8bf5e2b6fd4913f6c0747a6bc92d62592b75
ddmv19 <moronvillanuevadiego@outlook.com>
2024-03-19 10:39:48 -0500

    Styles

--- src/App.jsx
+++ src/App.jsx
@@ -4,8 +4,11 @@ function App() {
   return (
     <main className="">
       <h1
-        className="py-4 text-4xl text-center uppercase tracking-[.3em] border-b-4 border-slate-400 bg-gradient-to-r from-purple-300 to-indigo-600 bg-clip-text text-transparent"
-        style={{ fontFamily: 'Fredoka One' }}
+        className="py-4 text-4xl text-center uppercase tracking-[.3em] border-b-4 border-slate-400 bg-gradient-to-r from-purple-300 to-indigo-700 bg-clip-text text-transparent"
+        id="title"
+        style={{
+          fontFamily: 'Fredoka One',
+        }}
       >
         Memory Game
       </h1>
