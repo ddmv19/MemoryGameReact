5417e33bf31558093c4f1a520499053e9117bc46
ddmv19 <moronvillanuevadiego@outlook.com>
2024-03-24 23:56:01 -0500

    Add categories array

---
+++ src/utils/constants.js
@@ -0,0 +1,12 @@
+export const CATEGORIES = [
+  {
+    id: 'dragon-ball',
+    name: 'Dragon Ball',
+    border: 'border-orange-500',
+  },
+  {
+    id: 'demon-slayer',
+    name: 'Demon Slayer',
+    border: 'border-blue-500',
+  },
+]
