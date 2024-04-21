abadcee7e75f6d33dd31c2797951db24470b382d
ddmv19 <moronvillanuevadiego@outlook.com>
2024-04-20 20:14:54 -0500

    Add category and characters

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -3,10 +3,141 @@ export const CATEGORIES = [
     id: 'dragon-ball',
     name: 'Dragon Ball',
     border: 'border-orange-500',
+    characters: [
+      {
+        id: 'goku',
+        name: 'Goku',
+      },
+      {
+        id: 'goku',
+        name: 'Goku',
+      },
+      {
+        id: 'vegeta',
+        name: 'Vegeta',
+      },
+      {
+        id: 'gohan',
+        name: 'Gohan',
+      },
+      {
+        id: 'gohan',
+        name: 'Gohan',
+      },
+      {
+        id: 'piccolo',
+        name: 'Piccolo',
+      },
+      {
+        id: 'piccolo',
+        name: 'Piccolo',
+      },
+      {
+        id: 'vegeta',
+        name: 'Vegeta',
+      },
+      {
+        id: 'berus',
+        name: 'Berus',
+      },
+      {
+        id: 'berus',
+        name: 'Berus',
+      },
+    ],
   },
   {
     id: 'demon-slayer',
     name: 'Demon Slayer',
     border: 'border-blue-500',
+    characters: [
+      {
+        id: 'tanjiro',
+        name: 'Tanjiro',
+      },
+      {
+        id: 'tanjiro',
+        name: 'Tanjiro',
+      },
+      {
+        id: 'inosuke',
+        name: 'Inosuke',
+      },
+      {
+        id: 'inosuke',
+        name: 'Inosuke',
+      },
+      {
+        id: 'rengoku',
+        name: 'Rengoku',
+      },
+      {
+        id: 'rengoku',
+        name: 'Rengoku',
+      },
+      {
+        id: 'zenitsu',
+        name: 'Zenitsu',
+      },
+      {
+        id: 'zenitsu',
+        name: 'Zenitsu',
+      },
+      {
+        id: 'giyu',
+        name: 'Giyu',
+      },
+      {
+        id: 'giyu',
+        name: 'Giyu',
+      },
+    ],
+  },
+  {
+    id: 'jujutsu-kaisen',
+    name: 'Jujutsu Kaisen',
+    border: 'border-purple-500',
+    characters: [
+      {
+        id: 'gojo',
+        name: 'Gojo',
+      },
+      {
+        id: 'gojo',
+        name: 'Gojo',
+      },
+      {
+        id: 'itadori',
+        name: 'Itadori',
+      },
+      {
+        id: 'itadori',
+        name: 'Itadori',
+      },
+      {
+        id: 'nanami',
+        name: 'Nanami',
+      },
+      {
+        id: 'nanami',
+        name: 'Nanami',
+      },
+      {
+        id: 'sukuna',
+        name: 'Sukuna',
+      },
+      {
+        id: 'sukuna',
+        name: 'Sukuna',
+      },
+      {
+        id: 'geto',
+        name: 'Geto',
+      },
+      {
+        id: 'geto',
+        name: 'Geto',
+      },
+    ],
   },
 ]
