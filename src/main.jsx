28ea5374d4dd584edbc7ed4f218f553f441ee566
ddmv19 <moronvillanuevadiego@outlook.com>
2024-04-20 20:13:32 -0500

    Remove React Strict Mode

--- src/main.jsx
+++ src/main.jsx
@@ -1,13 +1,10 @@
-import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
 import { CategoriesProvider } from './context/categories.jsx'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <CategoriesProvider>
-      <App />
-    </CategoriesProvider>
-  </React.StrictMode>
+  <CategoriesProvider>
+    <App />
+  </CategoriesProvider>
 )
